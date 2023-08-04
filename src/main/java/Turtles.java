d5375255a6def3a4cc89ac0dc934f266aac5eb6c
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-04 18:16:53 -0400

    Factorial complete

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
+
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+
+    public long factorial(int n) {
+        if (n == 1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+
+
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,50 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+
+    @Test
+    public void factorialTest(){
+        //given
+        Turtles turtles = new Turtles();
+        Long expected = 120L;
+
+        //when
+        Long actual = turtles.factorial(5);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void factorialTest2(){
+        //given
+        Turtles turtles = new Turtles();
+        Long expected = 5040L;
+
+        //when
+        Long actual = turtles.factorial(7);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void factorialTest3(){
+        //given
+        Turtles turtles = new Turtles();
+        Long expected = 3628800L;
+
+        //when
+        Long actual = turtles.factorial(10);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
